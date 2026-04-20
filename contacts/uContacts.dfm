18b02e674460185a6ccbf5eea7e2689df23ec936
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2026-04-20 18:00:33 -0400

    Feat: DB Integration
    - Add SQLite connection and query components.
    - Create the DB and the table.
    - Implement insert, select, delete.
    - Remove the temporary list.

--- contacts/uContacts.dfm
+++ contacts/uContacts.dfm
@@ -3,7 +3,7 @@ object frmContacts: TfrmContacts
   Top = 0
   Caption = 'Delphi Contacts'
   ClientHeight = 441
-  ClientWidth = 436
+  ClientWidth = 429
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -14,42 +14,42 @@ object frmContacts: TfrmContacts
   OnDestroy = FormDestroy
   TextHeight = 15
   object lblTitle: TLabel
-    Left = 98
+    Left = 66
     Top = 64
     Width = 84
     Height = 15
     Caption = 'Delphi Contacts'
   end
   object lblName: TLabel
-    Left = 98
+    Left = 66
     Top = 99
     Width = 32
     Height = 15
     Caption = 'Name'
   end
   object lblPhoneNumber: TLabel
-    Left = 98
+    Left = 66
     Top = 133
     Width = 81
     Height = 15
     Caption = 'Phone Number'
   end
   object edtName: TEdit
-    Left = 192
+    Left = 160
     Top = 96
     Width = 143
     Height = 23
     TabOrder = 0
   end
   object edtPhoneNumber: TEdit
-    Left = 192
+    Left = 160
     Top = 125
     Width = 143
     Height = 23
     TabOrder = 1
   end
   object btnAdd: TButton
-    Left = 98
+    Left = 66
     Top = 376
     Width = 75
     Height = 25
@@ -58,7 +58,7 @@ object frmContacts: TfrmContacts
     OnClick = btnAddClick
   end
   object btnDelete: TButton
-    Left = 179
+    Left = 147
     Top = 376
     Width = 75
     Height = 25
@@ -67,7 +67,7 @@ object frmContacts: TfrmContacts
     OnClick = btnDeleteClick
   end
   object btnRefresh: TButton
-    Left = 260
+    Left = 228
     Top = 376
     Width = 75
     Height = 25
@@ -76,11 +76,24 @@ object frmContacts: TfrmContacts
     OnClick = btnRefreshClick
   end
   object lstContacts: TListBox
-    Left = 98
+    Left = 66
     Top = 168
     Width = 237
     Height = 186
     ItemHeight = 15
     TabOrder = 5
+  end
+  object fdConnContacts: TFDConnection
+    Params.Strings = (
+      'DriverID=SQLite'
+      'Database=contacts.db')
+    LoginPrompt = False
+    Left = 352
+    Top = 272
+  end
+  object fdQryContacts: TFDQuery
+    Connection = fdConnContacts
+    Left = 352
+    Top = 200
   end
 end
--- contacts/uContacts.pas
+++ contacts/uContacts.pas
@@ -4,7 +4,13 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, FireDAC.Stan.Intf,
+  FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf,
+  FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys,
+  FireDAC.VCLUI.Wait, FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf,
+  FireDAC.DApt, Data.DB, FireDAC.Comp.DataSet, FireDAC.Comp.Client,
+  FireDAC.Phys.SQLite, FireDAC.Phys.SQLiteDef, FireDAC.Stan.ExprFuncs,
+  FireDAC.Phys.SQLiteWrapper.Stat;
 
 type
   TfrmContacts = class(TForm)
@@ -17,6 +23,8 @@ type
     btnDelete: TButton;
     btnRefresh: TButton;
     lstContacts: TListBox;
+    fdConnContacts: TFDConnection;
+    fdQryContacts: TFDQuery;
     procedure FormCreate(Sender: TObject);
     procedure btnAddClick(Sender: TObject);
     procedure btnDeleteClick(Sender: TObject);
@@ -35,6 +43,23 @@ implementation
 
 {$R *.dfm}
 
+procedure TfrmContacts.FormCreate(Sender: TObject);
+begin
+  FContacts := TStringList.Create;
+
+  fdConnContacts.Connected := True;
+
+  fdQryContacts.Connection := fdConnContacts;
+
+  fdQryContacts.SQL.Text :=
+    'CREATE TABLE IF NOT EXISTS contacts (' +
+    'id INTEGER PRIMARY KEY AUTOINCREMENT, ' +
+    'name TEXT NOT NULL, ' +
+    'phone TEXT NOT NULL)';
+
+  fdQryContacts.ExecSQL;
+end;
+
 procedure TfrmContacts.btnAddClick(Sender: TObject);
 var
   Contact: string;
@@ -45,9 +70,19 @@ begin
     Exit;
   end;
 
-  Contact := Trim(edtName.Text) + ' - ' + Trim(edtPhoneNumber.Text);
-  FContacts.Add(Contact);
-  lstContacts.Items.Assign(FContacts);
+  if Trim(edtPhoneNumber.Text) = '' then
+  begin
+    ShowMessage('Enter a phone number');
+    Exit;
+  end;
+
+  fdQryContacts.SQL.Text :=
+  'INSERT INTO contacts (name, phone) VALUES (:name, :phone)';
+
+  fdQryContacts.ParamByName('name').AsString := Trim(edtName.Text);
+  fdQryContacts.ParamByName('phone').AsString := Trim(edtPhoneNumber.Text);
+
+  fdQryContacts.ExecSQL;
 
   edtName.Clear;
   edtPhoneNumber.Clear;
@@ -57,6 +92,10 @@ end;
 procedure TfrmContacts.btnDeleteClick(Sender: TObject);
 var
   Index: Integer;
+  ContactText: string;
+  SeparatorPos: Integer;
+  NameText: string;
+  PhoneText: string;
 begin
   Index := lstContacts.ItemIndex;
 
@@ -66,22 +105,49 @@ begin
     Exit;
   end;
 
-  FContacts.Delete(Index);
+  ContactText := lstContacts.Items[Index];
+  SeparatorPos := Pos(' - ', ContactText);
+
+  if SeparatorPos = 0 then
+  begin
+    ShowMessage('Invalid contact format');
+    Exit;
+  end;
+
+  NameText := Copy(ContactText, 1, SeparatorPos - 1);
+  PhoneText := Copy(ContactText, SeparatorPos + 3, Length(ContactText));
+
+  fdQryContacts.SQL.Text :=
+    'DELETE FROM contacts WHERE name = :name AND phone = :phone';
+
+  fdQryContacts.ParamByName('name').AsString := NameText;
+  fdQryContacts.ParamByName('phone').AsString := PhoneText;
+
+  fdQryContacts.ExecSQL;
 end;
 
 procedure TfrmContacts.btnRefreshClick(Sender: TObject);
 begin
-  lstContacts.Items.Assign(FContacts);
+  fdQryContacts.SQL.Text := 'SELECT name, phone FROM contacts';
+  fdQryContacts.Open;
+
+  lstContacts.Items.Clear;
+
+  while not fdQryContacts.Eof do
+  begin
+    lstContacts.Items.Add(
+      fdQryContacts.FieldByName('name').AsString + ' - ' +
+      fdQryContacts.FieldByName('phone').AsString
+    );
+    fdQryContacts.Next;
+  end;
+
+  fdQryContacts.Close;
 end;
 
 procedure TfrmContacts.FormDestroy(Sender: TObject);
 begin
   FContacts.Free;
-end;
-
-procedure TfrmContacts.FormCreate(Sender: TObject);
-begin
-  FContacts := TStringList.Create;
 end;
 
 end.
